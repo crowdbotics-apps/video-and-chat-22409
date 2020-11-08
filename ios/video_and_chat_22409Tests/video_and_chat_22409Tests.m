21eba59d8d71c008370e3c450ee862c68dd4e036
Crowdbotics <team@crowdbotics.com>
2020-11-08 20:42:25 +0000

    Initial commit.

---
+++ ios/video_and_chat_22409Tests/video_and_chat_22409Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface video_and_chat_22409 : XCTestCase
+
+@end
+
+@implementation video_and_chat_22409
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
